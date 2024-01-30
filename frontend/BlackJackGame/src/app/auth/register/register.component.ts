dccf6ce1498b468005859eb36907d5a9e56bbacd
Samuel Park <parksamuelsoung@gmail.com>
2024-01-29 18:36:22 -0600

    Configured frontend side http request.

--- frontend/BlackJackGame/src/app/auth/register/register.component.ts
+++ frontend/BlackJackGame/src/app/auth/register/register.component.ts
@@ -1,4 +1,7 @@
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-register',
@@ -6,5 +9,26 @@ import { Component } from '@angular/core';
   styleUrls: ['./register.component.css']
 })
 export class RegisterComponent {
+
+  url = 'http://localhost:3000/';
+
+  constructor (
+    private httpClient: HttpClient,
+    private formBuilder: FormBuilder,
+    private router: Router
+  ) {}
+
+  registerForm: FormGroup = this.formBuilder.group({
+    email: ['', Validators.required],
+    password: ['', Validators.required],
+    cash: ['', Validators.required]
+  });
+
+  register(): void {
+    if (this.registerForm.valid) {
+      this.httpClient.post(`${this.url}/register`, this.registerForm);
+    }
+  }
+  
 
 }
