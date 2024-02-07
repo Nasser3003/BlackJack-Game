c7b8b00223f007d12d89f8591bdebb8a718d9beb
Nasser <abdo.abdo3003@gmail.com>
2024-02-07 15:05:38 -0500

    split runner, fixed issue with card hidden status, persisted entity passing issue

--- src/main/java/play/blackjack/AppStarter.java
+++ src/main/java/play/blackjack/AppStarter.java
@@ -1,21 +1,40 @@
 package play.blackjack;
 
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.context.event.ApplicationReadyEvent;
 import org.springframework.context.event.EventListener;
 import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
 import play.blackjack.engine.Engine;
+import play.blackjack.model.Casino;
+import play.blackjack.model.Player;
+import play.blackjack.service.CasinoService;
+import play.blackjack.service.PlayerService;
 
+@NoArgsConstructor
+@AllArgsConstructor(onConstructor = @__(@Autowired))
 @Component
 public class AppStarter {
-
-    private final Engine engine;
-
-    public AppStarter(Engine engine) {
-        this.engine = engine;
-    }
+    private Engine engine;
+    private PlayerService playerService;
+    private CasinoService casinoService;
 
     @EventListener(ApplicationReadyEvent.class)
     public void startGame() {
+        Player playerRod = playerService.getPlayerByEmail("rod@gmail.com");
+
+        Casino casino = casinoService.getCasinoByName("Casino 1");
+
+        engine.addPlayer(playerRod);
+
+        engine.addPlayer(playerService.getPlayerByEmail("bot1@gmail.com"));
+        engine.addPlayer(playerService.getPlayerByEmail("bot2@gmail.com"));
+
+        engine.setCasino(casino);
+
         engine.start();
     }
 }
+// TODO login method should add the player to the players list
