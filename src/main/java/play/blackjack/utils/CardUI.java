121a711ca68b614fe51468193c31e16c270be86a
Nasser <abdo.abdo3003@gmail.com>
2024-02-07 20:32:08 -0500

    added login and register, and minimalist UI for them

---
+++ src/main/java/play/blackjack/utils/CardUI.java
@@ -0,0 +1,12 @@
+package play.blackjack.utils;
+
+import play.blackjack.cards.ENUMS.ECardRank;
+
+import java.util.HashMap;
+
+public class CardUI {
+    static final HashMap<ECardRank, String> CARD_ASCII = new HashMap<>();
+//    static {
+////        CARD_ASCII.put(ECardRank.ACE, );
+//    }
+}
