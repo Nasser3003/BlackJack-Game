ead53bb6efde7c636388e90727609dd7caf4ec33
Samuel Park <parksamuelsoung@gmail.com>
2024-02-10 07:51:44 -0600

    Need to create branch

--- src/main/java/play/blackjack/utils/CardUI.java
+++ src/main/java/play/blackjack/utils/CardUI.java
@@ -6,9 +6,14 @@ import play.blackjack.cards.ENUMS.ECardType;
 import java.util.HashMap;
 
 public class CardUI {
+    static final HashMap<ECardType, String> CARD_TYPE = new HashMap<>();
     static final HashMap<ECardRank, HashMap<ECardType, String>> CARD_ASCII = new HashMap<>();
-   static {
-       CARD_ASCII.put(ECardRank.ACE, "hi");
-       CARD_ASCII.put(ECardRank.ACE, "hi");
-   }
+    static {
+        CARD_TYPE.put(ECardType.CLUBS, "hi");
+        CARD_TYPE.put(ECardType.CLUBS, "hi");
+        CARD_TYPE.put(ECardType.CLUBS, "hi");
+        CARD_TYPE.put(ECardType.CLUBS, "hi");
+        CARD_ASCII.put(ECardRank.ACE, "hi");
+        CARD_ASCII.put(ECardRank.ACE, "hi");
+    }
 }
