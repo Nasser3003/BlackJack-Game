4a471837457d00bc3be3b2373771a00dbfe0bc44
Nasser <abdo.abdo3003@gmail.com>
2024-02-09 21:26:55 -0500

    improved code, implemented bet after every round

--- src/main/java/play/blackjack/Runner.java
+++ src/main/java/play/blackjack/Runner.java
@@ -4,43 +4,43 @@ import lombok.AllArgsConstructor;
 import lombok.NoArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Component;
+import play.blackjack.engine.Engine;
 import play.blackjack.model.Casino;
-import play.blackjack.model.Player;
 import play.blackjack.repository.CasinoRepository;
 import play.blackjack.service.AuthenticationService;
+import play.blackjack.service.CasinoService;
+import play.blackjack.service.PlayerService;
 
 @NoArgsConstructor
 @AllArgsConstructor(onConstructor = @__(@Autowired))
 @Component
 public class Runner implements CommandLineRunner {
     private CasinoRepository casinoRepository;
-    private PasswordEncoder encoder;
     private AuthenticationService authenticationService;
+    private Engine engine;
+    private PlayerService playerService;
+    private CasinoService casinoService;
 
 
     @Override
     public void run(String... args) {
-        if (!casinoRepository.findById(1L).isPresent()) {
-
-            Player playerRod = Player.builder()
-                    .email("bot1@gmail.com").password(encoder.encode("bot")).money(1000).build();
-            Player playerJosh = Player.builder()
-                    .email("bot2@gmail.com").password(encoder.encode("bot")).money(1000).build();
-            Player dealer = Player.builder()
-                    .email("rod@gmail.com").password(encoder.encode("rod")).money(1000).build();
+        if (!casinoRepository.findCasinoByName("Casino 1").isPresent()) {
 
             Casino casino = new Casino("Casino 1", 2000000000);
             casinoRepository.save(casino);
 
-            authenticationService.registerUser(dealer);
-            authenticationService.registerUser(playerRod);
-            authenticationService.registerUser(playerJosh);
+            authenticationService.register("bot1@gmail.com", "bot1", 10000L);
+            authenticationService.register("bot2@gmail.com", "bot2", 10000L);
 
         }
+
+        engine.addPlayer(playerService.getPlayerByEmail("bot1@gmail.com"));
+        engine.addPlayer(playerService.getPlayerByEmail("bot2@gmail.com"));
+
+        Casino casino = casinoService.getCasinoByName("Casino 1");
+        engine.setCasino(casino);
+
+        engine.start();
     }
 }
-// TODO if now i just need to print user experience
-// TODO implement user registration
-// TODO master the Cascade, and relationships.
